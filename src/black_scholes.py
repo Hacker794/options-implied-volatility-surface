066ea429537d0c7c08c426bae4556a278351c3f1
Dominic Ofili <dominicofili@gmail.com>
2026-09-07 19:00:25 +0100

    Put price call function created

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -24,10 +24,31 @@ def black_scholes_call(S, K, T, r, sigma):
 # Testing the function with example values
 
 S = 100  # Current stock price
-K = 120  # Strike price
+K = 100  # Strike price
 T = 1    # Time to expiration in years
 r = 0.05  # Risk-free interest rate
 sigma = 0.2  # Volatility of the underlying stock
 
 call_price = black_scholes_call(S, K, T, r, sigma)
 print(f"The Black-Scholes call option price is: {call_price:.2f}")
+
+# Puts price function
+
+def black_scholes_put(S, K, T, r, sigma):
+    d_1 = d1(S, K, T, r, sigma)
+    d_2 = d2(S, K, T, r, sigma)
+
+    return (
+        K * math.exp(-r * T) * norm.cdf(-d_2) - S * norm.cdf(-d_1)
+    )
+
+# Testing the put option price function with example values
+
+S = 100  # Current stock price
+K = 100  # Strike price
+T = 1    # Time to expiration in years
+r = 0.05  # Risk-free interest rate
+sigma = 0.2  # Volatility of the underlying stock
+
+put_price = black_scholes_put(S, K, T, r, sigma)
+print(f"The Black-Scholes put option price is: {put_price:.2f}")
