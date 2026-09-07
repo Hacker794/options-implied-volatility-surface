cc3513365bc37ab4d7e1545924981536553d708d
Dominic Ofili <dominicofili@gmail.com>
2026-09-07 18:44:06 +0100

    Checking that our model agrees with the fact that an increase in the strike price leads to should lead to a decrease in the price of the option, ceteris paribus

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -23,8 +23,8 @@ def black_scholes_call(S, K, T, r, sigma):
 
 # Testing the function with example values
 
-S = 120  # Current stock price
-K = 100  # Strike price
+S = 100  # Current stock price
+K = 120  # Strike price
 T = 1    # Time to expiration in years
 r = 0.05  # Risk-free interest rate
 sigma = 0.2  # Volatility of the underlying stock
