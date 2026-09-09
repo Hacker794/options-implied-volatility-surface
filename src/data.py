b2567c3b7e0c81ba07148fe589644861f2ffe613
Dominic Ofili <dominicofili@gmail.com>
2026-09-09 13:59:54 +0100

    Pulled options chain data

--- src/data.py
+++ src/data.py
@@ -1,4 +1,7 @@
 import yfinance as yf
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
 
 # Available option expiration dates for a given ticker symbol
 
@@ -10,3 +13,27 @@ if __name__ == "__main__":
     expiries = get_expiries("SPY")
 
     print(expiries)
+
+# Pull option chain data for a given ticker symbol and expiration date
+# Important columns in the option chain data include: strike, bid, ask, lastPrice, volume, openInterest, impliedVolatility, inTheMoney.
+
+def get_option_chain(ticker_symbol, expiry):
+    ticker = yf.Ticker(ticker_symbol)
+    chain = ticker.option_chain(expiry)
+
+    return chain.calls, chain.puts
+
+if __name__ == "__main__":
+    ticker_symbol = "SPY"
+    expiries = get_expiries(ticker_symbol)
+    first_expiry = expiries[0]
+
+    calls, puts = get_option_chain(ticker_symbol, first_expiry)
+
+    print("Expiration Date:", first_expiry)
+
+    print("\nCalls:")
+    print(calls.head())
+
+    print("\nPuts:")
+    print(puts.head())
